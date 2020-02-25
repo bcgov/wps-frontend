4255153d88e581d05692b3b1f33a65d48a7e940e
Nkuruba <nkuruba@VividSolutions.com>
2020-02-24 17:42:03 -0800

    Added a new component to select time range (a default time range - last 10 years is being used.)

---
+++ src/features/fwiCalculator/components/TimeRangeDropdown.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import { TextField } from '@material-ui/core'
+
+export const TimeRangeOptionsDropdown = () => {
+  const timeRangeOptions = ['Last 10 Years']
+
+  return (
+    <>
+      <Autocomplete
+        data-testid="time-range-dropdown"
+        options={timeRangeOptions}
+        style={{ width: 300 }}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Time Range"
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
+    </>
+  )
+}
