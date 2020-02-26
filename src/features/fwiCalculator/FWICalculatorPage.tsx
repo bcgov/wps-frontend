313c7a61e0e708eaa7d7c62cc14cc2f185ff118e
Nkuruba <nkuruba@VividSolutions.com>
2020-02-25 16:17:05 -0800

    Added an icon to navigate to weather stations map link

--- src/features/fwiCalculator/FWICalculatorPage.tsx
+++ src/features/fwiCalculator/FWICalculatorPage.tsx
@@ -51,7 +51,6 @@ export const FWICalculatorPage = () => {
           stations={stations}
           onStationsChange={onStationsChange}
         />
-
         <TimeRangeOptionsDropdown />
 
         <PercentileTextfield />
