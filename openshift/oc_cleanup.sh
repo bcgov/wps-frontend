4e8a8a2de824271a254a7b363872c6a821e86d87
Derek Roberts <derek.roberts@gmail.com>
2020-02-25 18:15:12 -0800

    oc_cleanup - basic dev project cleanup script

---
+++ openshift/oc_cleanup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#%
+#% OpenShift Build Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%
+#% Usage:
+#%
+#%   ${THIS_FILE} [PR_NUMBER] [apply]
+#%
+#% Examples:
+#%
+#%   Provide a PR number. Defaults to is a dry-run.
+#%   ${THIS_FILE} 0
+#%
+#%   Apply when satisfied.
+#%   ${THIS_FILE} 0 apply
+#%
+#%   Override variables at runtime.
+#%   GIT_BRANCH=branch PROJ_TOOLS=project PATH_BC=./bc.yaml ${THIS_FILE} 0 apply
+#%
+
+# Halt conditions (errors, unsets, non-zero pipes), field separator and verbosity
+#
+set -euo pipefail
+IFS=$'\n\t'
+[ ! "${VERBOSE:-}" == "true" ] || set -x
+
+# Parameters and environment vars
+#
+PR_NO=${1:-}
+APPLY=${2:-}
+source "$(dirname ${0})/envars"
+
+# Show help if no params
+#
+[ "${#}" -gt 0 ] || {
+	THIS_FILE="$(dirname ${0})/$(basename ${0})"
+	cat ${THIS_FILE} | grep "^#%" | sed -e "s|^#%||g" -e "s|\${THIS_FILE}|${THIS_FILE}|g"
+	exit
+}
+
+# Verify login
+#
+$(oc whoami &>/dev/null) || {
+	echo "Please verify oc login"
+	exit
+}
+
+# Process commands
+#
+OC_COMMAND="oc -n ${PROJ_DEPLOY} get all -o name -l app=${NAME}-pr-${PR_NO}"
+#
+[ "${APPLY}" != "apply" ] || {
+	OC_COMMAND="oc -n ${PROJ_DEPLOY} delete all -o name -l app=${NAME}-pr-${PR_NO}"
+}
+eval "${OC_COMMAND}"
+
+# Echo command
+#
+echo -e "\n${OC_COMMAND}\n"
