c89b927ea7ae81f3cee7609f30b68ea0b1e26202
Kyubin Han <kyubinhan11@gmail.com>
2020-02-26 14:28:41 -0800

    Minor change for deployment 3

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const local = 'http://localhost:8080'
 const isBuilt = process.env.NODE_ENV === 'production'
 // const baseURL = isBuilt ? '{{.Env.API_BASE_URL}}' : local
-const baseURL = isBuilt ? process.env.REACT_APP_API_BASE_URL : local
+// const baseURL = isBuilt ? process.env.REACT_APP_API_BASE_URL : local
 
 const instance = axios.create({
   baseURL: 'http://wps-api-dev-13-auzhsi.pathfinder.gov.bc.ca'
