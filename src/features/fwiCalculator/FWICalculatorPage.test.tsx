059e1fc56a047fc7d6e15b5fc4146f1bfd7a1c83
Kyubin Han <kyubinhan11@gmail.com>
2020-03-03 10:44:18 -0800

    Clean up FWICalculatorPage test file

--- src/features/fwiCalculator/FWICalculatorPage.test.tsx
+++ src/features/fwiCalculator/FWICalculatorPage.test.tsx
@@ -7,40 +7,13 @@ import { selectStationsReducer } from 'app/rootReducer'
 import { renderWithRedux } from 'utils/testUtils'
 import { WEATHER_STATION_MAP_LINK } from 'utils/constants'
 import { FWICalculatorPage } from 'features/fwiCalculator/FWICalculatorPage'
-import { PercentilesResponse } from 'api/percentileAPI'
 
 const mockAxios = new MockAdapter(axios)
-const mockStations = [
-  { code: 1, name: 'Station 1' },
-  { code: 2, name: 'Station 2' }
-]
-const mockPercentilesResponse: PercentilesResponse = {
-  stations: {
-    '1': {
-      FFMC: 94.95859222412109,
-      ISI: 15.589389991760253,
-      BUI: 157.5261016845703,
-      season: { start_month: 5, start_day: 1, end_month: 8, end_day: 31 },
-      years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
-      station_name: 'Station 1'
-    },
-    '2': {
-      FFMC: 90.95859222412109,
-      ISI: 10.589389991760253,
-      BUI: 159.5261016845703,
-      season: { start_month: 5, start_day: 1, end_month: 8, end_day: 31 },
-      years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
-      station_name: 'Station 2'
-    }
-  },
-  mean_values: {
-    FFMC: 93.95859222412109,
-    ISI: 12.589389991760253,
-    BUI: 158.5261016845703
-  },
-  year_range: { start: 2010, end: 2019 },
-  percentile: 90
-}
+
+import {
+  mockStations,
+  mockPercentilesResponse
+} from 'features/fwiCalculator/FWICalculatorPage.mock'
 
 afterEach(() => {
   mockAxios.reset()
