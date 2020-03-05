46cab5a4ce9c19e7a4cef8e5f1eed176ac77cf91
Derek Roberts <derek.roberts@gmail.com>
2020-03-04 17:10:36 -0800

    Simplified promotion script

--- openshift/oc_promote.sh
+++ openshift/oc_promote.sh
@@ -24,27 +24,21 @@ source "$(dirname ${0})/common/common"
 #
 SUFFIX_DEV="pr-${PR_NO}"
 SUFFIX_PROD="$(echo ${PROJ_PROD} | cut -d'-' -f2)"
-ISTAG_PROMOTE="${NAME}:${SUFFIX_DEV}"
-ISTAG_IS_PROD="${NAME}:${SUFFIX_PROD}"
+# The tag for the imagestream being imported from DEV to PROD
+TAG_IMPORT="${NAME}:${SUFFIX_DEV}"
+# The tag for labeling the most recent imagestreatag as PROD
+TAG_CURRENT="${NAME}:${SUFFIX_PROD}"
+# Long name for the DEV imagestreamtag to import to PROD
 SOURCE_IMG="docker-registry.default.svc:5000/${PROJ_TOOLS}/${NAME}-${SUFFIX_DEV}-s2i:latest"
 
-# Commands to import and label imagestreamtag as PROD (SUFFIX_PROD is the after-dash portion of PROJ_PROD)
-#
-# Check if the tag is available
-OC_TAG_VACANT="! oc -n ${PROJ_TOOLS} get istag ${ISTAG_PROMOTE} -o name &>/dev/null"
-# Delete that tag
-OC_TAG_DELETE="oc -n ${PROJ_TOOLS} delete istag ${ISTAG_PROMOTE}"
-# Pipe the first command into the second
-OC_TAG_VACATE="${OC_TAG_VACANT} || ${OC_TAG_DELETE}"
+# Create OpenShift commands to consume
 #
 # Import DEV image into an imagestream in PROD namespace
-OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${ISTAG_PROMOTE} --from=${SOURCE_IMG}"
+OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${TAG_IMPORT} --from=${SOURCE_IMG}"
 # Apply the PROD label to that imagestreamtag
-OC_IMG_TAG="oc -n ${PROJ_TOOLS} tag ${ISTAG_PROMOTE} ${ISTAG_IS_PROD}"
-
-# Command to process and promote imagestreamtag to PROD
-#
+OC_IMG_TAG="oc -n ${PROJ_TOOLS} tag ${TAG_IMPORT} ${TAG_CURRENT}"
 # Process a template (mostly variable substition)
+#
 OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_DC} -p NAME=${NAME} -p SUFFIX=${SUFFIX_DEV}"
 # Apply a template (can use --dry-run)
 OC_APPLY="oc -n ${PROJ_PROD} apply -f -"
@@ -54,7 +48,7 @@ OC_COMMAND="${OC_PROCESS} | ${OC_APPLY}"
 # Process commands
 #
 if [ "${APPLY}" ]; then
-	eval "${OC_TAG_VACATE}"
+	# Only delete the imagestreamtag if applying
 	eval "${OC_IMG_IMPORT}"
 	eval "${OC_IMG_TAG}"
 else
@@ -64,4 +58,4 @@ eval "${OC_COMMAND}"
 
 # Provide oc command instruction
 #
-display_helper "${OC_TAG_VACATE}" "${OC_IMG_IMPORT}" "${OC_IMG_TAG}" "${OC_COMMAND}"
+display_helper "${OC_IMG_IMPORT}" "${OC_IMG_TAG}" "${OC_COMMAND}"
