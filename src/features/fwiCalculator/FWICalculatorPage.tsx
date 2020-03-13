f57fc02cfd72d5133631e59126a0a8ca49a714f8
Kyubin Han <kyubinhan11@gmail.com>
2020-03-13 15:38:48 -0700

    Change FWICalculatorPage.tsx back to previous

--- src/features/fwiCalculator/FWICalculatorPage.tsx
+++ src/features/fwiCalculator/FWICalculatorPage.tsx
@@ -60,9 +60,7 @@ export const FWICalculatorPage = () => {
   return (
     <div data-testid="fwi-calculator-page">
       <PageHeader title="Predictive Services Unit" />
-
       <PageTitle title="FWI Calculator" />
-
       <Container>
         <WeatherStationsDropdown
           stations={stations}
