ab70e3c912a37c7656fee97651fd98db34a87590
Kyubin Han <kyubinhan11@gmail.com>
2020-03-27 10:17:09 -0700

    Do not use oc_templates.sh

--- openshift/scripts/oc_deploy.sh
+++ openshift/scripts/oc_deploy.sh
@@ -25,17 +25,31 @@ source "$(dirname ${0})/common/common"
 #
 PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 
+# Process a template (mostly variable substition)
+#
+OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_DC} -p NAME=${NAME_APP} -p SUFFIX=${SUFFIX}"
+
+# Apply a template (apply or use --dry-run)
+#
+OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run"
+
+# Deploy the app using the deployment config
+# 
 OC_DEPLOY="oc -n ${PROJ_TARGET} rollout latest dc/${NAME_OBJ}"
 OC_LOG="oc -n ${PROJ_TARGET} logs -f dc/${NAME_OBJ}"
-
 if [ ! "${APPLY}" ]; then
   OC_DEPLOY="${OC_DEPLOY} --dry-run"
   OC_LOG=""
 fi
 
+# Execute commands
+#
+eval "${OC_PROCESS}"
+eval "${OC_PROCESS} | ${OC_APPLY}"
 eval "${OC_DEPLOY}"
 eval "${OC_LOG}"
 
 # Provide oc command instruction
 #
-display_helper "${OC_DEPLOY}" "${OC_LOG}"
+display_helper "${OC_PROCESS} | ${OC_APPLY}" "${OC_DEPLOY}" "${OC_LOG}"
