42b7d64aecd550d3ff7d5b0d2410fa2c54219418
Kyubin Han <kyubinhan11@gmail.com>
2020-03-27 12:13:07 -0700

    And test it

--- src/features/fwiCalculator/FWICalculatorPage.tsx
+++ src/features/fwiCalculator/FWICalculatorPage.tsx
@@ -59,7 +59,7 @@ export const FWICalculatorPage = () => {
 
   return (
     <div data-testid="fwi-calculator-page">
-      <PageHeader title="Predictive Services Unit" />
+      <PageHeader title="Predictive Services Test" />
       <PageTitle title="FWI Calculator" />
       <Container>
         <WeatherStationsDropdown
