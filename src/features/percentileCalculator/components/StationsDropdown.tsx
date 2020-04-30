08bd078e9d2a5ed02d3cfcf017323268763e5d6e
Andrea Williams <andrea.williams@gov.bc.ca>
2020-04-30 11:10:53 -0700

    Add View Map text to button - styling still required

--- src/features/percentileCalculator/components/StationsDropdown.tsx
+++ src/features/percentileCalculator/components/StationsDropdown.tsx
@@ -69,6 +69,7 @@ export const WeatherStationsDropdown = (props: Props) => {
             onClick={onMapIconClick}
           >
             <MapIcon />
+            View Map
           </IconButton>
         </Tooltip>
       </div>
