4d3806ee09331afb2549d9aaf4799179b4196e90
Kyubin Han <kyubinhan11@gmail.com>
2020-05-29 15:42:37 -0700

    Working graph display for actual & model wx data

--- src/features/fireWeather/components/WxDataGraph.tsx
+++ src/features/fireWeather/components/WxDataGraph.tsx
@@ -15,6 +15,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { datetimeInPDT } from 'utils/date'
 import { ModelValue } from 'api/modelAPI'
 import { MODEL_VALUE_DECIMAL } from 'utils/constants'
+import { ReadingValue } from 'api/readingAPI'
 
 const useStyles = makeStyles({
   graph: {
@@ -46,10 +47,16 @@ const formatTooltipValue = (
   return value
 }
 
-const getDateRange = (values: ModelValue[]) => {
+const sortByDatetime = (a: WxData, b: WxData) => {
+  const a1 = new Date(a.datetime)
+  const b1 = new Date(b.datetime)
+  return a1 > b1 ? 1 : a1 < b1 ? -1 : 0
+}
+
+const getDateRange = (data: WxData[]) => {
   const days: string[] = []
   const map: { [k: string]: boolean } = {}
-  values.forEach(v => {
+  data.forEach(v => {
     const dt = datetimeInPDT(v.datetime, 'Do MMM')
     if (!map[dt]) {
       map[dt] = true
@@ -60,49 +67,82 @@ const getDateRange = (values: ModelValue[]) => {
   return days
 }
 
-interface Props {
-  values: ModelValue[] | undefined
+interface WxData {
+  datetime: string
+  temp?: number
+  modelTemp?: number
+  rh?: number
+  modelRh?: number
 }
 
-const WxGraphByStation = ({ values }: Props) => {
-  const classes = useStyles()
+interface Props {
+  modelValues: ModelValue[] | undefined
+  readingValues: ReadingValue[] | undefined
+}
 
-  if (!values || values.length === 0) {
+const WxDataGraph = ({ modelValues, readingValues }: Props) => {
+  const classes = useStyles()
+  const wxData: WxData[] = []
+
+  if (!modelValues && !readingValues) {
     return null
   }
 
-  const dateRange = getDateRange(values)
+  if (readingValues && readingValues.length > 0) {
+    readingValues.forEach(v => {
+      wxData.push({
+        datetime: v.datetime,
+        temp: v.temperature,
+        rh: v.relative_humidity
+      })
+    })
+  }
+
+  if (modelValues && modelValues.length > 0) {
+    modelValues.forEach(v => {
+      wxData.push({
+        datetime: v.datetime,
+        modelTemp: v.temperature,
+        modelRh: v.relative_humidity
+      })
+    })
+  }
+
+  wxData.sort(sortByDatetime)
+
+  const dateRange = getDateRange(wxData)
 
   return (
     <div className={classes.graph} data-testid="weather-graph-by-station">
       <Typography className={classes.title} component="div" variant="subtitle2">
-        GDPS 3 hourly data with interpolated noon values (PDT, UTC-7):
+        Past 5 days of hourly readings and GDPS 3 hourly model with interpolated noon
+        values (PDT, UTC-7):
       </Typography>
 
       <ResponsiveContainer width="100%" minHeight={300}>
-        <LineChart data={values} margin={{ top: 10, right: 20, bottom: 5, left: 0 }}>
+        <LineChart data={wxData} margin={{ top: 10, right: 20, bottom: 5, left: 0 }}>
           <XAxis
-            dataKey="datetime"
             allowDataOverflow
+            dataKey="datetime"
             ticks={dateRange}
             tickFormatter={formatXAxis}
           />
           <YAxis
             yAxisId="left"
+            allowDataOverflow
             orientation="left"
             type="number"
             unit="°"
             domain={['auto', 'auto']}
-            allowDataOverflow
             label={{ value: 'Temp (°C)', angle: -90, position: 'insideLeft' }}
           />
           <YAxis
             yAxisId="right"
+            allowDataOverflow
             orientation="right"
             type="number"
             unit="%"
             domain={[0, 100]}
-            allowDataOverflow
             label={{
               value: 'RH',
               angle: -270,
@@ -110,18 +150,38 @@ const WxGraphByStation = ({ values }: Props) => {
             }}
           />
           <Line
+            connectNulls
+            strokeWidth={1.5}
+            type="monotone"
             yAxisId="left"
-            type="natural"
             name="Temp"
-            dataKey="temperature"
-            stroke="#8884d8"
+            dataKey="temp"
+            stroke="crimson"
           />
           <Line
+            connectNulls
+            strokeWidth={1.5}
+            type="monotone"
             yAxisId="right"
-            type="natural"
             name="RH"
-            dataKey="relative_humidity"
-            stroke="#82ca9d"
+            dataKey="rh"
+            stroke="royalblue"
+          />
+          <Line
+            connectNulls
+            type="monotone"
+            yAxisId="left"
+            name="Model Temp"
+            dataKey="modelTemp"
+            stroke="indianred"
+          />
+          <Line
+            connectNulls
+            type="monotone"
+            yAxisId="right"
+            name="Model RH"
+            dataKey="modelRh"
+            stroke="dodgerblue"
           />
           <CartesianGrid stroke="#ccc" strokeDasharray="1 1" />
           <Tooltip labelFormatter={formatTooltipLabel} formatter={formatTooltipValue} />
@@ -132,4 +192,4 @@ const WxGraphByStation = ({ values }: Props) => {
   )
 }
 
-export default React.memo(WxGraphByStation)
+export default React.memo(WxDataGraph)
