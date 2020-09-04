c4bd1b5852d754d929f062fc738c596f74b65db6
Sybrand Strauss <sybrand.strauss@gmail.com>
2020-09-04 13:11:04 -0700

    using config.js

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,12 @@
-// Environment variables that will be swapped during runtime by Caddy
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '{{.Env.API_BASE_URL}}'
+const env = (window as { [key: string]: any })['env']
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || './api'
 export const HIDE_DISCLAIMER = process.env.REACT_APP_HIDE_DISCLAIMER
 export const KC_AUTH_URL =
-  process.env.REACT_APP_KEYCLOAK_AUTH_URL || '{{.Env.KC_AUTH_URL}}'
-export const KC_REALM = process.env.REACT_APP_KEYCLOAK_REALM || '{{.Env.KC_REALM}}'
-export const KC_CLIENT = process.env.REACT_APP_KEYCLOAK_CLIENT || '{{.Env.KC_CLIENT}}'
+  process.env.REACT_APP_KEYCLOAK_AUTH_URL || env?.REACT_APP_KEYCLOAK_AUTH_URL
+export const KC_REALM =
+  process.env.REACT_APP_KEYCLOAK_REALM || env?.REACT_APP_KEYCLOAK_REALM
+export const KC_CLIENT =
+  process.env.REACT_APP_KEYCLOAK_CLIENT || env?.REACT_APP_KEYCLOAK_CLIENT
 
 export const WEATHER_STATION_MAP_LINK =
   'https://governmentofbc.maps.arcgis.com/apps/webappviewer/index.html?id=c36baf74b74a46978cf517579a9ee332'
