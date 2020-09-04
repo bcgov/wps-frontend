075f0c6530d7c7f2d1e8312e9ab78ea3dc7b0872
Kyubin Han <kyubinhan11@gmail.com>
2020-09-04 15:51:31 -0700

    Use interface declaration merging for Window

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,11 @@
-/* eslint-disable */
-const env = (window as { [key: string]: any })['env']
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || './api'
 export const HIDE_DISCLAIMER = process.env.REACT_APP_HIDE_DISCLAIMER
 export const KC_AUTH_URL =
-  process.env.REACT_APP_KEYCLOAK_AUTH_URL || env?.REACT_APP_KEYCLOAK_AUTH_URL
+  process.env.REACT_APP_KEYCLOAK_AUTH_URL || window.env.REACT_APP_KEYCLOAK_AUTH_URL
 export const KC_REALM =
-  process.env.REACT_APP_KEYCLOAK_REALM || env?.REACT_APP_KEYCLOAK_REALM
+  process.env.REACT_APP_KEYCLOAK_REALM || window.env.REACT_APP_KEYCLOAK_REALM
 export const KC_CLIENT =
-  process.env.REACT_APP_KEYCLOAK_CLIENT || env?.REACT_APP_KEYCLOAK_CLIENT
+  process.env.REACT_APP_KEYCLOAK_CLIENT || window.env.REACT_APP_KEYCLOAK_CLIENT
 
 export const WEATHER_STATION_MAP_LINK =
   'https://governmentofbc.maps.arcgis.com/apps/webappviewer/index.html?id=c36baf74b74a46978cf517579a9ee332'
