a1df436d7d6664f14507fe72ff3cf9920f25c147
Kyubin Han <kyubinhan11@gmail.com>
2020-10-22 12:37:17 -0700

    Update AuthWrapper not to depend on Cypress

--- src/features/auth/AuthWrapper.tsx
+++ src/features/auth/AuthWrapper.tsx
@@ -5,24 +5,17 @@ import { selectAuthentication } from 'app/rootReducer'
 import { authenticate } from 'features/auth/slices/authenticationSlice'
 
 interface Props {
-  shouldAuthenticate: boolean
   children: React.ReactElement
 }
 
 // TODO: Turn this into High order function later
-const AuthWrapper = ({ children, shouldAuthenticate }: Props) => {
+const AuthWrapper = ({ children }: Props) => {
   const dispatch = useDispatch()
   const { isAuthenticated, authenticating, error } = useSelector(selectAuthentication)
 
   useEffect(() => {
-    if (shouldAuthenticate) {
-      dispatch(authenticate())
-    }
-  }, [dispatch, shouldAuthenticate])
-
-  if (!shouldAuthenticate) {
-    return children
-  }
+    dispatch(authenticate())
+  }, [dispatch])
 
   if (error) {
     return <div>{error}</div>
